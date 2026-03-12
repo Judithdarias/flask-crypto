2990e43b88cd3c4f3aaf074c2ef488fab7de03b3
Judith Darias <judithdariascurbelo98@gmail.com>
2026-03-12 12:36:54 +0100

    Rutas base de inicio compra y estado

--- appchanges/routes.py
+++ appchanges/routes.py
@@ -6,3 +6,11 @@ def register_routes(app):
     @app.get("/")
     def home():
         return render_template("index.html")
+
+    @app.get("/purchase")
+    def purchase():
+        return render_template("purchase.html")
+
+    @app.get("/status")
+    def status():
+        return render_template("status.html")
